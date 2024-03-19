d20c933e65fa25b9c41bb33f7964efa25f61093d
KyVighnesh <110763997+KyVighnesh@users.noreply.github.com>
2024-03-19 15:49:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,101 +1,107 @@
-const express = require("express")
-const app = express();
-const  multipart = require('connect-multiparty');
-const path=require("path");
-const cors = require("cors")
+    const express = require("express")
+    const app = express();
+    const  multipart = require('connect-multiparty');
+    const path=require("path");
+    const cors = require("cors")
+    require('dotenv').config()
 
 
 
 
-app.use(express.json())
-app.use(cors())
+
+    app.use(express.json())
+    app.use(cors())
 
 
-const fs = require("fs")
+    const fs = require("fs")
 
-const  multipartMiddleware = multipart({ uploadDir: `${path.join(__dirname,"./uploads")}` });
-
-
-
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
-const ffmpeg = require('fluent-ffmpeg');
-ffmpeg.setFfmpegPath(ffmpegPath);
-
-var arg = ""
+    const  multipartMiddleware = multipart({ uploadDir: `${path.join(__dirname,"./uploads")}` });
 
 
-app.post("/upload",multipartMiddleware,(req,res)=> {
+    const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+    const ffmpeg = require('fluent-ffmpeg');
+    ffmpeg.setFfmpegPath(ffmpegPath);
 
-    
-
-    fs.readdir('./uploads',(err,data)=> {
-        // data.forEach((ele)=> {
-        //     console.log(ele)
-        //     arg = ele
-        //     var base = new String(ele).substring(ele.lastIndexOf("/") + 1)
-
-    // if(base.lastIndexOf(".") != -1) {
-    //     base = base.substring(0,base.lastIndexOf("."))
-    //     console.log(base)
-    // }
+    var arg = ""
 
 
-        // })
+    app.post("/upload",(req,res,next)=>{
 
-        for(let i = 0; i<data.length;i++) {
-
-            arg = data[data.length-1]
-            var base = new String(arg).substring(arg.lastIndexOf("/") + 1)
-
+        if(process.env.CREATED == "false"){
+            fs.mkdirSync(path.join(__dirname,'./uploads'))
+            process.env.CREATED = true;
         }
 
-        ffmpeg(`./uploads/${arg}`).
-        toFormat('mp3').
-        saveToFile('converted.mp3',()=> {
+        next()
+    },multipartMiddleware,(req,res)=> {
 
-        }) 
+            fs.readdir('./uploads',(err,data)=> {
+                // data.forEach((ele)=> {
+                //     console.log(ele)
+                //     arg = ele
+                //     var base = new String(ele).substring(ele.lastIndexOf("/") + 1)
+    
+            // if(base.lastIndexOf(".") != -1) {
+            //     base = base.substring(0,base.lastIndexOf("."))
+            //     console.log(base)
+            // }
+    
+    
+                // })
+    
+                for(let i = 0; i<data.length;i++) {
+    
+                    arg = data[data.length-1]
+                    var base = new String(arg).substring(arg.lastIndexOf("/") + 1)
+    
+                }
+    
+                ffmpeg(`./uploads/${arg}`).
+                toFormat('mp3').
+                saveToFile('converted.mp3',()=> {
+    
+                }) 
+                
+            })
+    
         
+        
+        res.json({
+            "message":"success"
+        })
+
+            
+            
+            
     })
 
-    
 
-    
-    res.json({
-        "message":"success"
+    app.get('/converted',(req,res)=> {
+        res.sendFile(path.join(__dirname, 'converted.mp3'))
     })
 
-        
-        
-        
-})
+    app.put('/unlink',(req,res)=> {
 
-
-app.get('/converted',(req,res)=> {
-    res.sendFile(path.join(__dirname, 'converted.mp3'))
-})
-
-app.put('/unlink',(req,res)=> {
-
-    fs.readdir("./uploads", (err, files) => {
-        if (err) throw err;
-      
-        for (const file of files) {
-          fs.unlink(path.join("./uploads", file), (err) => {
+        fs.readdir("./uploads", (err, files) => {
             if (err) throw err;
-          });
-        }
-      });
-    fs.unlink(path.join(__dirname, 'converted.mp3'),()=> {
-        console.log("ok")
+        
+            for (const file of files) {
+            fs.unlink(path.join("./uploads", file), (err) => {
+                if (err) throw err;
+            });
+            }
+        });
+        fs.unlink(path.join(__dirname, 'converted.mp3'),()=> {
+            console.log("ok")
+        })
+        res.json({
+            message:"done"
+        })
     })
-    res.json({
-        message:"done"
+
+
+
+
+    app.listen("8090",()=> {
+        console.log("server started on 8090")
     })
-})
-
-
-
-
-app.listen("8090",()=> {
-    console.log("server started on 8090")
-})
