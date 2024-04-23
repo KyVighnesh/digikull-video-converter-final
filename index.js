d93c0361f194d6bad0524fd9cf90432ad6aa4d1a
Vighneswar <vighneshwar.s.10@gmail.comm>
2024-04-24 03:34:44 +0530

    fix-policy

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
     app.options('*', (req, res) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization,file');
         res.status(200).send();
       });
 
