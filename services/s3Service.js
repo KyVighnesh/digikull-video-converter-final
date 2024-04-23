c1eb0a308c18d062f5e50763e71f6c4ce9b21216
Vighneswar <vighneshwar.s.10@gmail.comm>
2024-04-24 01:37:19 +0530

    .ENV data changed

--- services/s3Service.js
+++ services/s3Service.js
@@ -7,8 +7,8 @@ const path=require("path");
 
 
 const s3 = new AWS.S3({
-    accessKeyId: "AKIAWJXUR4N6GLU72NAQ",
-    secretAccessKey: "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"
+    accessKeyId: process.env.ID,
+    secretAccessKey: process.env.KEY
 })
 
 const fileUploadS3=(file)=>{
