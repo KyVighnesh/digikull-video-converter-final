35a830cf8bc91b1f93dd433c235aa02e0150f48c
Vighneswar <vighneshwar.s.10@gmail.comm>
2024-04-24 02:03:34 +0530

    cors policy changed

--- index.js
+++ index.js
@@ -12,7 +12,9 @@
 
     
     app.use(express.json())
-    app.use(cors())
+    app.use(cors({
+        origin:'https://digikull-video-converter-final.onrender.com'
+    }))
 
 
     const fs = require("fs")
